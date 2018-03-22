be93c795d4471c247c096c98061c74c75c71b8e8
ozuntini <olivier.zuntini@netcourrier.com>
2018-03-22 19:23:26 +0100

    Reprise des tests de fonctionnement

--- set-iso.py
+++ set-iso.py
@@ -4,16 +4,6 @@
 # http://github.com/jim-easterbrook/python-gphoto2
 # Copyright (C) 2015  Jim Easterbrook  jim@jim-easterbrook.me.uk
 #
-# This program is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
-#
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-#
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
@@ -26,8 +16,8 @@ import gphoto2 as gp
 
 def main():
     # use Python logging
-    #logging.basicConfig(format='%(levelname)s: %(name)s: %(message)s', level=logging.WARNING)
-    #gp.check_result(gp.use_python_logging())
+    logging.basicConfig(format='%(levelname)s: %(name)s: %(message)s', level=logging.WARNING)
+    gp.check_result(gp.use_python_logging())
     
     # open camera connection
     camera = gp.check_result(gp.gp_camera_new())
@@ -50,11 +40,13 @@ def main():
     shutterspeed = gp.check_result(gp.gp_widget_get_child_by_name(config, 'shutterspeed'))
     aperture = gp.check_result(gp.gp_widget_get_child_by_name(config, 'aperture'))
     capturetarget = gp.check_result(gp.gp_widget_get_child_by_name(config, 'capturetarget'))
+    
     # check value in range
     count = gp.check_result(gp.gp_widget_count_choices(iso))
     if valIso < 0 or valIso >= count:
         print('Parameter out of range')
         return 1
+    
     # set value
     valIso = gp.check_result(gp.gp_widget_get_choice(iso, valIso))
     gp.check_result(gp.gp_widget_set_value(iso, valIso))
